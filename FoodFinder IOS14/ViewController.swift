a3d9ab116673dc52781939290cf87bbe3ef51d4a
Matias C. Rojas Z <rojasz.ma@gmail.com>
2020-11-11 20:33:55 +0100

    Initial Commit

---
+++ FoodFinder IOS14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodFinder IOS14
+//
+//  Created by Matias Rojas Zuñiga on 11/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
